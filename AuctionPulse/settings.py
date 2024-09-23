3a955bc348c2a781ab0726c3667dd7e2289a25e1
Danielyilma <yilmad752@gmail.com>
2024-09-23 22:22:25 +0300

    adding cors configration

--- AuctionPulse/settings.py
+++ AuctionPulse/settings.py
@@ -20,6 +20,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",  # Frontend URL
+    "http://127.0.0.1:5500",
+]
+
 
 # Application definition
 
@@ -38,9 +43,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -170,6 +177,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # custom user model
 AUTH_USER_MODEL = 'UserAccountManager.User'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
